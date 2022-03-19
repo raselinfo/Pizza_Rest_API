c67c5187b9e69615fcaf979d45ad6e30703596be
Rasel Hossain <raselinfo52@gmail.com>
2022-03-19 19:16:18 +0600

    read me file added

--- middleware/auth/isAdminMiddleware.js
+++ middleware/auth/isAdminMiddleware.js
@@ -1,8 +1,20 @@
 import CustomErrorHandler from "../../service/CustomErrorHandler"
-export const isAdminMiddleware = (req, res, next) => {
-    if (!(req.user.role.toLowerCase() === "admin")) {
+import UserModel from "../../model/user/User.model"
+export const isAdminMiddleware = async (req, res, next) => {
+    let { _id } = req.user
+    // Todo: check if the user exists in the database
+    let user
+    try {
+        user = await UserModel.findById({ _id: _id })
+        if (!user) {
+            return next(CustomErrorHandler.notfound("User Not Found"))
+        }
+    } catch (err) {
+        return next(err)
+    }
+    // Todo: check if the request come from admin
+    if (user.role.toLowerCase() !== "admin") {
         return next(CustomErrorHandler.unauthorized("unauthorize! Admin Required"))
     }
-
     return next()
 }
