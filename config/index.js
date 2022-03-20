1f92b91da215e68262b53cfa6aeca184e91e0b63
Rasel Hossain <raselinfo52@gmail.com>
2022-03-20 11:06:11 +0600

    update

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-export const { APP_PORT, DB_URI, DEBUG_MODE, JWT_ACCESS_SECRET, JWT_REFRESH_SECRET, APP_URL}=process.env
+export const { APP_PORT, DB_URI, DEBUG_MODE, JWT_ACCESS_SECRET, JWT_REFRESH_SECRET, APP_URL } = process.env
 
